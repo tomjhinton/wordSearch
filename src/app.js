afc6acdb3f7c6258c071c988bbddcbec1ee1f25d
Tom Hinton <tomjhinton@gmail.com>
2020-03-25 21:25:25 +0000

    setup

---
+++ src/app.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+
+
+import Main from './components/Main'
+import './style.scss'
+
+class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+
+
+  render() {
+    return (
+
+      <Router>
+        <main>
+
+          <Switch>
+
+            <Route path="/" component={Main} />
+
+          </Switch>
+
+        </main>
+
+      </Router>
+
+
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
