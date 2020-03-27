969cb4d74e56f6f8970bc24ce9fdac1baf0975da
Tom Hinton <tomjhinton@gmail.com>
2020-03-27 08:42:40 +0000

    inital setup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,1 +1,66 @@
 String.fromCharCode(97+Math.floor(Math.random() * 26))
+import React from 'react'
+
+
+import axios from 'axios'
+
+
+class Main extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      data: {},
+      error: ''
+
+    }
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.mouseMove = this.mouseMove.bind(this)
+
+
+
+
+
+
+  }
+
+
+  componentDidMount(){
+
+
+
+  }
+
+  componentDidUpdate(){
+
+
+
+  }
+
+  mouseMove(e){
+
+    //console.log(e)
+
+    // this.setState({bass: `${e.screenX /100000} ${e.screenY /100000} `, scale: `${e.screenY /2}` })
+  }
+
+
+
+
+  render() {
+
+
+
+    return (
+      <div onMouseMove={this.mouseMove} className="body">
+
+      hiya
+
+      </div>
+
+
+
+
+    )
+  }
+}
+export default Main
