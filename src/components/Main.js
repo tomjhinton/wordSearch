813f3cd0429eae1bdb80406ac0b40a0e2b326b1e
Tom Hinton <tomjhinton@gmail.com>
2020-03-27 13:22:28 +0000

    random letter grid

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,17 @@
-String.fromCharCode(97+Math.floor(Math.random() * 26))
+
 import React from 'react'
 
 
 import axios from 'axios'
 
-
+let arr = []
 class Main extends React.Component{
   constructor(){
     super()
     this.state = {
       data: {},
-      error: ''
+      error: '',
+      size: 10
 
     }
     this.componentDidMount = this.componentDidMount.bind(this)
@@ -25,7 +26,8 @@ class Main extends React.Component{
 
 
   componentDidMount(){
-
+    this.create()
+    console.log(arr)
 
 
   }
@@ -43,18 +45,35 @@ class Main extends React.Component{
     // this.setState({bass: `${e.screenX /100000} ${e.screenY /100000} `, scale: `${e.screenY /2}` })
   }
 
-
+  create(){
+    arr = []
+      for(let i=0; i<this.state.size;i++){
+        arr.push([])
+       for(let j=0; j<this.state.size;j++){
+         arr[i].push(String.fromCharCode(97+Math.floor(Math.random() * 26)))
+       }
+      }
+      this.setState({array: arr })
+  }
 
 
   render() {
 
 
-
+      console.log(this.state)
     return (
+
       <div onMouseMove={this.mouseMove} className="body">
 
         <div className='board'>
-
+        {arr.map(x=>{
+          return(
+          x.map(y=>{
+            return(
+              <div key={new Date().getTime()*Math.random()} className='letter'>{y}</div>
+            )
+          }))
+        })}
         </div>
 
       </div>
